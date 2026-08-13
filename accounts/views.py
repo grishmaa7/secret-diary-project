b37cf1e439a60ffdea63f4f319f58a7197fcf596
barsa-pudasaini <barsapudasaini6@gmail.com>
2026-08-13 22:52:54 +0545

    Add token authentication and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
-from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework import generics, serializers
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .serializers import RegisterSerializer
 
@@ -7,3 +10,36 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+
+        if user is None:
+            raise serializers.ValidationError(
+                "Invalid username or password."
+            )
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return {
+            'token': token.key,
+            'username': user.username
+        }
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
