a0846be91c24d46275d33fe480d317d00b0dd68f
grishmaa7 <gisme7@gmail.com>
2026-08-13 22:23:00 +0545

    Add Entry serializer and ViewSet

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Entry
+from .serializers import EntrySerializer
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    serializer_class = EntrySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Entry.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
