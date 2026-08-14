4a99913edc2fd177708fbd68be150437640b9eaa
grishmaa7 <gisme7@gmail.com>
2026-08-14 07:54:12 +0545

    Add categories views and urls

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category
 
-# Create your views here.
+
+def categories_page(request):
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        'categories.html',
+        {'categories': categories}
+    )
+
+
+def create_category(request):
+    if request.method == 'POST':
+        Category.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description']
+        )
+
+        return redirect('categories')
+
+    return render(request, 'category_form.html')
--- secret_diary/urls.py
+++ secret_diary/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from journal.views import home, entries_page, create_entry, entry_edit, entry_delete
+from categories.views import categories_page, create_category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +16,8 @@ urlpatterns = [
     path('entries/new/', create_entry, name='create_entry'),
     path('entries/<int:entry_id>/edit/', entry_edit, name='entry_edit'),
     path('entries/<int:entry_id>/delete/', entry_delete, name='entry_delete'),
+    path('categories/', categories_page, name='categories'),
+    path('categories/new/', create_category, name='create_category'),
 
     # Authentication pages
     path('', include('accounts.urls')),
