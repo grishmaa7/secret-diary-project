e5a92b0771e48a43d9b7651ca8d469d86da91315
grishmaa7 <gisme7@gmail.com>
2026-08-14 06:12:57 +0545

    Add journal API routes

--- journal/urls.py
+++ journal/urls.py
@@ -1,8 +1,15 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EntryViewSet
+
+from .views import EntryViewSet, ReflectionViewSet
+
 
 router = DefaultRouter()
 
 router.register('entries', EntryViewSet, basename='entry')
+router.register('reflections', ReflectionViewSet, basename='reflection')
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
